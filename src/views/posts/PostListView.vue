c421799d318009b9877af7bedc9e74e8c1729b21
seorang42 <seorang42@naver.com>
2025-01-21 01:01:00 +0900

    3-2 Vue Router 학습 : 게시판 UI 만들기

---
+++ src/views/posts/PostListView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2>게시글 목록</h2>
+    <hr class="my-4" />
+    <div class="row g-3">
+      <div v-for="post in posts" :key="post.id" class="col-4">
+        <PostItem
+          :title="post.title"
+          :content="post.content"
+          :created-at="post.createdAt"
+          @click="goPage(post.id)"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { getPosts } from '@/api/posts';
+import PostItem from '@/components/posts/PostItem.vue';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const posts = ref([]);
+
+const fetchPosts = () => {
+  posts.value = getPosts();
+};
+fetchPosts();
+const goPage = id => {
+  // router.push(`/posts/${id}`)
+  // http://localhost:3000/posts/2?searchText=hello#world
+  router.push({
+    name: 'PostDetail',
+    params: {
+      id,
+    },
+    query: {
+      searchText: 'hello',
+    },
+    hash: '#world',
+  });
+};
+</script>
+
+<style lang="scss" scoped></style>
