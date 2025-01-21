1d877431a84d3f24c2598414d2375b173d156106
seorang42 <seorang42@naver.com>
2025-01-21 17:17:42 +0900

    5-1 공통 컴포넌트 분리 (based on Vue.js 3 spec)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,16 +4,25 @@
     <p>{{ $route.path }}</p>
     <p>{{ $route.name }}</p>
     <button class="btn btn-primary" @click="goAboutPage">About으로 이동</button>
+    <hr class="my-4" />
+    <AppGrid :items="items" v-slot="{ item }" col-class="col-6">
+      <AppCard>{{ item }}</AppCard>
+    </AppGrid>
   </div>
 </template>
 
 <script setup>
+import AppCard from '@/components/AppCard.vue';
+import AppGrid from '@/components/AppGrid.vue';
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
 const goAboutPage = () => {
   router.push('/about');
 };
+
+const items = ref(['사과', '딸기', '포도', '바나나']);
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -2,41 +2,28 @@
   <div>
     <h2>게시글 수정</h2>
     <hr class="my-4" />
-    <form @submit.prevent="edit">
-      <div class="mb-3">
-        <label for="title" class="form-label">제목</label>
-        <input
-          v-model="form.title"
-          type="text"
-          class="form-control"
-          id="exampleFormControlInput1"
-        />
-      </div>
-      <div class="mb-3">
-        <label for="content" class="form-label">내용</label>
-        <textarea
-          v-model="form.content"
-          class="form-control"
-          id="content"
-          rows="3"
-        ></textarea>
-      </div>
-      <div class="pt-4">
+    <PostForm
+      v-model:title="form.title"
+      v-model:content="form.content"
+      @submit.prevent="edit"
+    >
+      <template #actions>
         <button
           type="button"
-          class="btn btn-outline-danger me-2"
+          class="btn btn-outline-danger"
           @click="goDetailPage"
         >
           취소
         </button>
         <button class="btn btn-primary">수정</button>
-      </div>
-    </form>
+      </template>
+    </PostForm>
   </div>
 </template>
 
 <script setup>
 import { getPostsById, updatePost } from '@/api/posts';
+import PostForm from '@/components/posts/PostForm.vue';
 import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
