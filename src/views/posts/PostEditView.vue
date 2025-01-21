eb3bcc0a6ae081eab232f66ced81c107b81d7e09
seorang42 <seorang42@naver.com>
2025-01-21 18:05:20 +0900

    6-1 Transition 컴포넌트

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -19,10 +19,12 @@
       </template>
     </PostForm>
   </div>
+  <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" />
 </template>
 
 <script setup>
 import { getPostsById, updatePost } from '@/api/posts';
+import AppAlert from '@/components/AppAlert.vue';
 import PostForm from '@/components/posts/PostForm.vue';
 import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
@@ -47,13 +49,28 @@ fetchPost();
 const edit = async () => {
   try {
     await updatePost(id, { ...form.value });
-    router.push({ name: 'PostDetail', params: { id } });
+    // router.push({ name: 'PostDetail', params: { id } });
+    vAlert('수정이 완료되었습니다!', 'success');
   } catch (error) {
     console.error(error);
+    vAlert('네트워크 오류');
   }
 };
 
 const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } });
+
+// alert
+const showAlert = ref(false);
+const alertMessage = ref('');
+const alertType = ref('');
+const vAlert = (message, type = 'error') => {
+  showAlert.value = true;
+  alertMessage.value = message;
+  alertType.value = type;
+  setTimeout(() => {
+    showAlert.value = false;
+  }, 2000);
+};
 </script>
 
 <style lang="scss" scoped></style>
