9bc0ddeb27901845ef10fa3b78ad2c8ddd8c464c
seorang42 <seorang42@naver.com>
2025-01-22 00:38:15 +0900

    10-2 Pinia 기초 : state, getters, actions

--- src/main.js
+++ src/main.js
@@ -4,21 +4,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import globalComponents from './plugins/global-components';
-// import focus from './directive/focus';
-
-const app = createApp(App);
-app.use(globalComponents);
-// app.directive('focus', focus);
-app.use(router);
-app.use(globalDirective);
-app.use(dayjs);
-app.mount('#app');
 import 'bootstrap/dist/js/bootstrap.js';
 import globalDirective from './plugins/global-directive';
 import dayjs from './plugins/dayjs';
+import { createPinia } from 'pinia';
 
-/* console.log('MODE: ', import.meta.env.MODE);
-console.log('BASE_URL: ', import.meta.env.BASE_URL);
-console.log('PROD: ', import.meta.env.PROD);
-console.log('DEV: ', import.meta.env.DEV);
-console.log('VITE_APP_API_URL :', import.meta.env.VITE_APP_API_URL); */
+const app = createApp(App);
+app.use(globalComponents);
+app.use(router);
+app.use(globalDirective);
+app.use(dayjs);
+app.use(createPinia());
+app.mount('#app');
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,14 +6,26 @@
     <button class="btn btn-primary" @click="$router.push('/')">
       Home으로 이동
     </button>
+    <h2>Store</h2>
+    <p>counter: {{ counter }}</p>
+    <p>doubleCounter: {{ doubleCount }}</p>
+    <p>doubleCounterPlusOne: {{ doubleCountPlusOne }}</p>
+    <button @click="increment()">Click</button>
   </div>
 </template>
 
 <script setup>
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 console.log(route.path);
+
+const store = useCounterStore();
+const { counter, doubleCount, doubleCountPlusOne } = storeToRefs(store);
+const { increment } = store;
+counter.value = 100;
 </script>
 
 <style lang="scss" scoped></style>
