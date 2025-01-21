96dbddee192925b8544ad90a2f410edb74c77b74
seorang42 <seorang42@naver.com>
2025-01-22 01:02:36 +0900

    11-1 네비게이션 가드

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -14,10 +14,14 @@
     <AppError v-if="removeError" :message="removeError.message" />
     <div class="row g-2">
       <div class="col-auto">
-        <button class="btn btn-outline-dark">이전글</button>
+        <button class="btn btn-outline-dark" @click="router.push('/posts/10')">
+          이전글
+        </button>
       </div>
       <div class="col-auto">
-        <button class="btn btn-outline-dark">다음글</button>
+        <button class="btn btn-outline-dark" @click="router.push('/posts/11')">
+          다음글
+        </button>
       </div>
       <div class="col-auto me-auto"></div>
       <div class="col-auto">
@@ -58,7 +62,7 @@ import { useAlert } from '@/composables/alert';
 import { useNumber } from '@/composables/number';
 import { useAxios } from '@/hooks/useAxios';
 import { computed, toRefs } from 'vue';
-import { useRouter } from 'vue-router';
+import { onBeforeRouteLeave, onBeforeRouteUpdate, useRouter } from 'vue-router';
 
 const props = defineProps({
   id: [String, Number],
@@ -101,6 +105,20 @@ const goEditPage = () =>
   router.push({ name: 'PostEdit', params: { id: props.id } });
 
 const { vSuccess } = useAlert();
+
+onBeforeRouteUpdate(() => {
+  console.log('onBeforeRouteUpdate');
+});
+onBeforeRouteLeave(() => {
+  console.log('onBeforeRouteLeave');
+});
+</script>
+<script>
+export default {
+  beforeRouteEnter() {
+    console.log('beforeRouteEnter');
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
